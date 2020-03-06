bd33f759bb810d772b39366d64160e517cfb338f
Ursula Rodgers <ursularodgers@OVO4045.local>
2020-03-06 10:59:01 +0000

    data, models and theme

---
+++ theme.js
@@ -0,0 +1,19 @@
+const palette = {
+    white: '#fff',
+    offWhite: '#fcfcfc',
+    lightGrey:'#e5e3e6',
+    medGrey: '#ADAAAA',
+    darkGrey: '#716F6F',
+    lilac: '#c085ed',
+    orange: '#f5a442'
+}
+
+export const colors = {
+    primaryText: palette.white,
+    primaryHeaderBackground: palette.medGrey,
+    primaryBackground: palette.lightGrey,
+    secondaryBackground: palette.offWhite,
+    mealItemDefault: palette.lilac,
+    mealsTabColor: palette.darkGrey,
+    favTabColor: palette.orange
+}
